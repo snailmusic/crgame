c09847cbfbf594ea501d7188be9e533077e2dcb0
Dlol <dravennadas@gmail.com>
2024-07-31 16:58:34 -0500

    tell the user abt the crimes theyre commiting (keys)

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,13 @@
 use bevy::prelude::*;
 use leafwing_input_manager::{plugin::InputManagerPlugin, prelude::*, Actionlike, InputManagerBundle};
 
+const KEY_LIST: [Actions;4] = [
+    Actions::Key1,
+    Actions::Key2,
+    Actions::Key3,
+    Actions::Key4
+];
+
 pub fn run() {
     App::new()
         .add_plugins(DefaultPlugins)
@@ -10,7 +17,7 @@ pub fn run() {
         .run();
 }
 
-#[derive(Actionlike, PartialEq, Eq, Hash, Clone, Copy, Debug, Reflect)]
+#[derive(Component, Actionlike, PartialEq, Eq, Hash, Clone, Copy, Debug, Reflect)]
 enum Actions {
     Key1,
     Key2,
@@ -18,7 +25,7 @@ enum Actions {
     Key4
 }
 
-fn setup(mut commands: Commands) {
+fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
     let input_map = InputMap::new([
         (Actions::Key1, KeyCode::KeyD),
         (Actions::Key2, KeyCode::KeyF),
@@ -30,25 +37,57 @@ fn setup(mut commands: Commands) {
 
     commands
         .spawn(InputManagerBundle::with_map(input_map));
+
+    commands.spawn((
+        SpriteBundle {
+            texture: asset_server.load("Key_A.png"),
+            transform: Transform { translation: Vec3 { x: -128., y: -200., z: 0.0 }, ..default() },
+            ..default()
+        },
+        Actions::Key1
+    ));
+
+    commands.spawn((
+        SpriteBundle {
+            texture: asset_server.load("Key_B.png"),
+            transform: Transform { translation: Vec3 { x: -64., y: -200., z: 0.0 }, ..default() },
+            ..default()
+        },
+        Actions::Key2
+    ));
+
+    commands.spawn((
+        SpriteBundle {
+            texture: asset_server.load("Key_B.png"),
+            transform: Transform { translation: Vec3 { x: 0., y: -200., z: 0.0 }, ..default() },
+            ..default()
+        },
+        Actions::Key3
+    ));
+
+    commands.spawn((
+        SpriteBundle {
+            texture: asset_server.load("Key_A.png"),
+            transform: Transform { translation: Vec3 { x: 64., y: -200., z: 0.0 }, ..default() },
+            ..default()
+        },
+        Actions::Key4
+    ));
 }
 
 fn check_input(
-    query: Query<&ActionState<Actions>>
+    query: Query<&ActionState<Actions>>,
+    mut keys: Query<(&mut Transform, &Actions)>
 ) {
     let action_state = query.single();
-    if action_state.just_pressed(&Actions::Key1) {
-        println!("1");
-    }
-
-    if action_state.just_pressed(&Actions::Key2) {
-        println!("2");
-    }
-
-    if action_state.just_pressed(&Actions::Key3) {
-        println!("3");
-    }
-
-    if action_state.just_pressed(&Actions::Key4) {
-        println!("4");
+    
+    for (mut trans, key) in keys.iter_mut() {
+        if action_state.pressed(key) {
+            println!("yippee!! {:?}", key);
+            trans.scale = Vec3::new(0.9, 0.9, 1.);
+        }
+        else {
+            trans.scale = Vec3::new(1., 1., 1.);
+        }
     }
 }
